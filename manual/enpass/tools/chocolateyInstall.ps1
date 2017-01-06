d0bb3351af344502970dd24daa9b0af54e1797dc
w0ndersp00n <wicky@rampersad.me>
2017-01-06 10:53:57 +0100

    Update chocolateyInstall.ps1

--- manual/enpass/tools/chocolateyInstall.ps1
+++ manual/enpass/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'Enpass'
-$url         = 'https://dl.sinew.in/windows/setup/5-4-0-4/Enpass_Setup_5.4.exe'
-$checksum    = '7968B055B6164635856C9D9977C2BDE568C2B6EBCBEA76CF9D96882EB5A3066E'
+$url         = 'https://dl.sinew.in/windows/setup/5-4-1/Enpass_Setup.exe'
+$checksum    = '992289535760560587DF712C3DA7CDF214E24294D61E23D61BE3383F1FD66934'
 
 $packageArgs = @{
   packageName            = $packageName
