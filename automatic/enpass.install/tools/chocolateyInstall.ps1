ece5448557613a8c250a09c9d6884e54a4b694ef
w0ndersp00n <wicky@rampersad.me>
2017-04-25 21:29:58 +0200

    Automatic Package updater for Enpass

---
+++ automatic/enpass.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'Enpass'
+$url         = 'https://dl.sinew.in/windows/setup/5-5-3/Enpass_Setup.exe'
+$checksum    = 'cf9813d203a7660481eac723acdf0959860fc53eea6943097a4de7dd6c29cc33'
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url
+  silentArgs             = '/S'
+  checksum               = $checksum
+  checksumType           = 'sha256'
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
+}
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/enpass.install/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://www.enpass.io/release-notes/windowspc/'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+    $version = $download_page.ParsedHtml.documentElement.getElementsByTagName('h2')[0].innerHTML
+
+    $re  = $version -match "\d+(\.\d+)+"
+
+    $version = $Matches[0].Replace(".","-")
+
+    $url = "https://dl.sinew.in/windows/setup/"+$version+"/Enpass_Setup.exe"
+
+    return @{ URL = $url; Version = $version.Replace("-",".") }
+}
+
+update
