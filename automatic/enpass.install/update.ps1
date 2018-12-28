9c5c6691c344adf73a0d520832e7cdb257e96c99
w0ndersp00n <wicky@rampersad.me>
2018-12-28 23:02:08 +0100

    Update update.ps1
    Get version from URL

--- automatic/enpass.install/update.ps1
+++ automatic/enpass.install/update.ps1
@@ -15,13 +15,13 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
     # $version = $download_page.ParsedHtml.documentElement.getElementsByTagName('h3')[0].innerHTML
 
-    # $version = $Matches[0]
     # $versionUrl = $Matches[0].Replace(".","-")
 
     $url = $download_page.ParsedHtml.getElementById('web_windows_desktop') | ForEach-Object { $_.getElementsByTa
 gName('a') } | Where-Object { $_.className -eq 'btn btn_enpass_line btn_enpass_color' } |  Select-Object -Expand href
 
     $version  = $url -match "\d+(\.\d+)+"
+    $version = $Matches[0]
 
     return @{ URL = $url; Version = $version }
 }
