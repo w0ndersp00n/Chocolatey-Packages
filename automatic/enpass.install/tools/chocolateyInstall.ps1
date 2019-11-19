82dfff33779547e0cc016136479214233668fc42
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2019-11-20 02:09:45 +0900

    update enpass to 6.3.0.579

--- automatic/enpass.install/tools/chocolateyInstall.ps1
+++ automatic/enpass.install/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'Enpass'
-$url         = 'https://dl.enpass.io/stable/windows/setup/6.0.0.230/Enpass-setup.exe'
-$checksum    = '43d06787fe3a3a66a26c8b5b119b826b2cc903b38cb335b8867862ce84eed09a'
+$url         = 'https://dl.enpass.io/stable/windows/setup/6.3.0.579/Enpass-setup.exe'
+$checksum    = 'F28BE1DE41F15F970EDBACAE809029CD0232F03A3CAC205E61E238A77475C68A'
 
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
   url                    = $url
-  silentArgs             = '/quiet'
+  silentArgs             = '/quiet /norestart'
   checksum               = $checksum
   checksumType           = 'sha256'
   validExitCodes         = @(0)
